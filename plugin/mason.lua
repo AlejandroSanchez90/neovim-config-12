b8e94690e764eb612637a26718ef860f0dfd7d0d
Alejandro Sanchez <Alejandro_Sanchez@eogresources.com>
2026-04-05 08:58:30 -0500

    mason auto install

--- plugin/mason.lua
+++ plugin/mason.lua
@@ -1,11 +1,28 @@
--- ◍ tailwindcss-language-server (keywords: css)
--- ◍ vtsls (keywords: javascript, typescript)
--- ◍ emmet-language-server (keywords: emmet)
--- ◍ eslint-lsp (keywords: javascript, typescript)
--- ◍ prettier (keywords: angular, css, flow, graphql, html, json, jsx, javascript, less, markdown, scss, typescript, vue, yaml)
-
 vim.pack.add({
-	"https://github.com/mason-org/mason.nvim",
+  'https://github.com/mason-org/mason.nvim',
 })
 
-require("mason").setup()
+require('mason').setup()
+
+local ensure_installed = {
+  'tailwindcss-language-server',
+  'vtsls',
+  'emmet-language-server',
+  'eslint-lsp',
+  'prettier',
+}
+
+vim.api.nvim_create_autocmd('VimEnter', {
+  once = true,
+  callback = function()
+    local registry = require('mason-registry')
+    registry.refresh(function()
+      for _, name in ipairs(ensure_installed) do
+        local pkg = registry.get_package(name)
+        if not pkg:is_installed() then
+          pkg:install()
+        end
+      end
+    end)
+  end,
+})
