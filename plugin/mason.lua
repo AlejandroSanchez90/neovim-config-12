ce63145fff362044dd01474c5189efc9ed262b3a
Alejandro Sanchez <alejandrosanchez9010@gmail.com>
2026-05-03 18:19:22 -0500

    added c++ to mason

--- plugin/mason.lua
+++ plugin/mason.lua
@@ -1,28 +1,30 @@
 vim.pack.add({
-  'https://github.com/mason-org/mason.nvim',
+	"https://github.com/mason-org/mason.nvim",
 })
 
-require('mason').setup()
+require("mason").setup()
 
 local ensure_installed = {
-  'tailwindcss-language-server',
-  'vtsls',
-  'emmet-language-server',
-  'eslint-lsp',
-  'prettier',
+	"tailwindcss-language-server",
+	"vtsls",
+	"emmet-language-server",
+	"eslint-lsp",
+	"prettier",
+	"clangd",
+	"clang-format",
 }
 
-vim.api.nvim_create_autocmd('VimEnter', {
-  once = true,
-  callback = function()
-    local registry = require('mason-registry')
-    registry.refresh(function()
-      for _, name in ipairs(ensure_installed) do
-        local pkg = registry.get_package(name)
-        if not pkg:is_installed() then
-          pkg:install()
-        end
-      end
-    end)
-  end,
+vim.api.nvim_create_autocmd("VimEnter", {
+	once = true,
+	callback = function()
+		local registry = require("mason-registry")
+		registry.refresh(function()
+			for _, name in ipairs(ensure_installed) do
+				local pkg = registry.get_package(name)
+				if not pkg:is_installed() then
+					pkg:install()
+				end
+			end
+		end)
+	end,
 })
