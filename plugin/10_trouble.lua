9ab1e3b92163a00640ab93215f6f33bb35eabdc2
Alejandro Sanchez <Alejandro_Sanchez@eogresources.com>
2026-06-11 09:02:57 -0500

    trouble keymaps

--- plugin/10_trouble.lua
+++ plugin/10_trouble.lua
@@ -13,3 +13,11 @@ end, { desc = 'Quickfix (Trouble)' })
 vim.keymap.set('n', '<leader>xl', function()
   vim.cmd('Trouble loclist toggle')
 end, { desc = 'Location List (Trouble)' })
+
+vim.keymap.set('n', '<leader>xx', function()
+  vim.cmd('Trouble diagnostics toggle filter.buf=0')
+end, { desc = 'Buffer Diagnostics (Trouble)' })
+
+vim.keymap.set('n', '<leader>xX', function()
+  vim.cmd('Trouble diagnostics toggle')
+end, { desc = 'Diagnostics (Trouble)' })
